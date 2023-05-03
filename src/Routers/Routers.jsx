55137c63d42322d67792ba7a504d8743617b9024
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-03 07:53:17 +0600

    Created React app and setup React router dom and some component

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../Pages/Home/Home";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      // errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        }
+        
+      ],
+    },
+  ]);
+
+  export default router;
