9a47ef5fd3873d9526ff2fffd87899a27bea62d6
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-03 08:54:28 +0600

    Footer part is added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import Navber from '../Shared/Navber/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navber></Navber>
             <Outlet></Outlet>
-            
+            <Footer></Footer>
+
         </div>
     );
 };
