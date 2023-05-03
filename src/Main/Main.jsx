a3957af8c0f4b621d2cbb53ff56941a9579bb3e6
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-03 08:30:49 +0600

    Navber part is added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Navber from '../Shared/Navber/Navber';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
+            <Navber></Navber>
+            <Outlet></Outlet>
             
         </div>
     );
---
+++ src/Shared/Navber/Navber.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Container, Dropdown, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../assets/logo.jpg'
+import profile from "../../assets/profile.webp"
+import { Link } from 'react-router-dom';
+
+const Navber = () => {
+    return (
+        <div>
+            <Navbar style={{backgroundColor:"#0000005e"}} collapseOnSelect expand="lg" fixed="top" variant="">
+                <Container>
+                    <img style={{ width: "70px", height: "50px" }} src={logo}></img>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Link className='text-decoration-none text-white fw-bold px-3' to="/">Home</Link>
+                            <Link className='text-decoration-none text-white fw-bold px-3' to="/blog">Blog</Link>
+                            <Link className='text-decoration-none text-white fw-bold px-3' to="/login">Login</Link>
+                        </Nav>
+                        <Nav>
+                            <Dropdown>
+                                <Dropdown.Toggle id="dropdown-button-dark-example1" variant="">
+                                    <img style={{ width: "40px" }} className='rounded-circle' src={profile} alt=""></img>
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu variant="dark">
+                                    <Dropdown.Item href="#/action-1" active>
+                                        Log Out
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Navber;
