39d5f535cbcc4df6d3a53a1fe6d3a4a3428389ec
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-05 12:39:05 +0600

    Recipe Cards part is added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Banner from '../../Shared/Banner/Banner';
 import ChefCards from '../../Shared/ChefCards/ChefCards';
 import Contact from '../../Shared/Contact/Contact';
+import RecipeCard from '../../Shared/RecipeCard/RecipeCard';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <ChefCards></ChefCards>
+            <RecipeCard></RecipeCard>
             <Contact></Contact>
         </div>
     );
---
+++ src/Shared/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+const RecipeCard = () => {
+    const [chefAllDatas, setChefAllData] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:7000/chefAllData')
+            .then(res => res.json())
+            .then(data => setChefAllData(data))
+    }, [])
+    console.log(chefAllDatas);
+    return (
+        <Container >
+            <h2 className='fw-bold text-center pb-5'>More Recipes</h2>
+            <Row>
+                {
+                    chefAllDatas.map(chefAllData =>
+                        <div className='col-lg-4 col-sm-12' key={chefAllData.id}>
+                            <Col >
+                                {
+                                    chefAllData.recipes.map(recipe =>
+                                        <div key={recipe.name}>
+                                            <Card className='mb-4'>
+                                                <Card.Img variant="top" src={recipe.image}/>
+                                                <Card.Body>
+                                                    <Card.Title >{recipe.name}</Card.Title>
+                                                    <Card.Text>Views :&nbsp; 
+                                                    {recipe.rating} K
+                                                    </Card.Text>
+                                                    <Button variant="primary">Show details</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+
+                                    )
+                                }
+                            </Col>
+                        </div>
+                    )
+                }
+            </Row>
+        </Container>
+    );
+};
+
+export default RecipeCard;
