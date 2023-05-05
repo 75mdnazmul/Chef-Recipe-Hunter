bc30604634d60c6d7f1c9b56312c2d7567f8166f
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-05 16:12:50 +0600

    navigation loading is complete

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Navber from '../Shared/Navber/Navber';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
-    
+        const navigation = useNavigation()
     return (
         <div>
             <Navber></Navber>
+            <div>{navigation.state === 'loading' ? 'Loading.......' : ''}</div>
                 <Outlet></Outlet>
             <Footer></Footer>
 
